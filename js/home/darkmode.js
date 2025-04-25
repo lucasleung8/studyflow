d0fb3d5645c0aae4b7a279fc7590dfcb72e75ad1
Dragonzxdd <raymondzhongbob@gmail.com>
2025-04-25 19:09:43 -0400

    local storage for dark mode

--- js/home/darkmode.js
+++ js/home/darkmode.js
@@ -3,6 +3,12 @@ window.addEventListener("load", function (event) {
     let themeSwitch = document.getElementById("themeSwitch");
     themeSwitch.addEventListener("click", () => switchTheme());
 
+    //Check for last saved setting
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme === "dark") {
+        document.body.classList.add("darkmode");
+    }
+
     /**
      * Switches theme to either light/dark
      */
@@ -10,9 +16,11 @@ window.addEventListener("load", function (event) {
         if (document.body.classList.contains("darkmode")) {
             //Switch to lightmode
             document.body.classList.remove("darkmode");
+            localStorage.setItem("theme", "light");
         } else {
             //Switch to darkmode
             document.body.classList.add("darkmode");
+            localStorage.setItem("theme", "dark");
         }
     }
 });
